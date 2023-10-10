4f65cc216a03b6f0b428a72ecfd7b40593c3fcc9
a-martinma <a.martinma.2020@alumnos.urjc.es>
2023-10-10 18:32:42 +0200

    Más cambios

--- app/src/main/java/com/example/mainactivity/GameActivity.java
+++ app/src/main/java/com/example/mainactivity/GameActivity.java
@@ -10,6 +10,12 @@ public class GameActivity extends AppCompatActivity {
     private int puntuacion = 0;
     private TextView puntosTextView;
 
+    Pregunta1 pregunta1;
+    Pregunta2 pregunta2;
+    Pregunta3 pregunta3;
+    Pregunta4 pregunta4;
+    Pregunta5 pregunta5;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,15 @@ public class GameActivity extends AppCompatActivity {
 
         puntosTextView = findViewById(R.id.puntosTextView);
         puntosTextView.setText("Puntuación: " + puntuacion);
+
+        pregunta1 = new Pregunta1();
+        pregunta2 = new Pregunta2();
+        pregunta3 = new Pregunta3();
+        pregunta4 = new Pregunta4();
+        pregunta5 = new Pregunta5();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments,pregunta1).commit();
+
     }
 
     public void aumentarPuntuacion() {
