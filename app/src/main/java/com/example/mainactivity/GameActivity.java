4e5397f8e7d4d14d598fb65dd7e984f87b16b9dc
a-martinma <a.martinma.2020@alumnos.urjc.es>
2023-10-25 09:57:45 +0200

    Pequeño último cambio

--- app/src/main/java/com/example/mainactivity/GameActivity.java
+++ app/src/main/java/com/example/mainactivity/GameActivity.java
@@ -3,11 +3,9 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
